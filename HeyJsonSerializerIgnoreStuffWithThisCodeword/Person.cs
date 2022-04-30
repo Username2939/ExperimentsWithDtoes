c818201844d520a60829abe9000d2c1c9bb67f77
Michele C <michele.cociancich@gmail.com>
2022-04-30 02:16:29 +0200

    -

---
+++ HeyJsonSerializerIgnoreStuffWithThisCodeword/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace HeyJsonSerializerIgnoreStuffWithThisCodeword
+{
+    public record Person(
+        string? FirstName,
+        string? LastName,
+        int? Age,
+        string? SocialSecurity,
+        string? BankAccountPassword
+        );
+}
---
+++ HeyJsonSerializerIgnoreStuffWithThisCodeword/Program.cs
@@ -0,0 +1,23 @@
+﻿using HeyJsonSerializerIgnoreStuffWithThisCodeword;
+using Newtonsoft.Json;
+
+var person = new Person("Molly",              // First Name
+                        "Mouk",               // Last Name
+                        21,                   // Age
+                        "SomeSensitiveData",  // Social Security Number
+                        "MoreSensitiveData"   // Bank Account Password
+                        );
+
+// insert some work on the domain object here ...
+// now sensitive data must be shaved off to send it to the presentation layer ...
+
+person = person with { SocialSecurity = null, BankAccountPassword = null };
+
+var settings = new JsonSerializerSettings // tell serializer to ignore null values
+{
+    DefaultValueHandling = DefaultValueHandling.Ignore,
+    NullValueHandling = NullValueHandling.Ignore
+};
+
+Console.WriteLine(JsonConvert.SerializeObject(person, settings)); // returns a JSON with only FirstName, LastName, and Age
+Console.ReadKey();
